254914e51c404d91a5553f0b05ebca2c551c63b8
Tim Quimbo <tquimbo1@gmail.com>
2021-11-26 16:01:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
+import Cell from './Cell.js'
+
 
 ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
